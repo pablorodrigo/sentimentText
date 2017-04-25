739aa5e4094e8139b41f7dad2b0c5c6bc2e2310f
pablorodrigo <wfpablo@hotmail.com>
2017-04-24 21:24:47 -0400

    Correcting exception

--- sourceCode/java/sentimentTwitter/src/main/java/sentimentTwitter/JsonFiles.java
+++ sourceCode/java/sentimentTwitter/src/main/java/sentimentTwitter/JsonFiles.java
@@ -1,47 +1,40 @@
 package sentimentTwitter;
 
 import java.io.FileReader;
-import java.io.IOException;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 public class JsonFiles {
 
 	@SuppressWarnings("unchecked")
-	public JSONArray jsonFileRead(String fileLocation) {
-
-		fileLocation = "/home/pablo/workspace/trabalhoUninorte/sentimentTwitter/src/jsonFiles/100twitters.json";
+	public JSONArray jsonFileRead(String fileLocation) throws Exception {
 
 		// novo array de json
 		JSONArray newJsonArray = new JSONArray();
 
 		JSONObject newObject_for_newJsonArray = new JSONObject();
 
-		try {
-			JSONParser parser = new JSONParser();
+		JSONParser parser = new JSONParser();
 
-			// pega todos os objetos do arquivo
-			Object obj = parser.parse(new FileReader(fileLocation));
+		// pega todos os objetos do arquivo
+		Object obj = parser.parse(new FileReader(fileLocation));
 
-			// pega os objetos do arquivo e trasforma no array json
-			JSONArray fileJsonArray = (JSONArray) obj;
+		// pega os objetos do arquivo e trasforma no array json
+		JSONArray fileJsonArray = (JSONArray) obj;
 
-			for (int i = 0; i < fileJsonArray.size(); i++) {
+		for (int i = 0; i < fileJsonArray.size(); i++) {
 
-				JSONObject twitter = (JSONObject) fileJsonArray.get(i);
+			JSONObject twitter = (JSONObject) fileJsonArray.get(i);
 
-				newObject_for_newJsonArray.put("text", twitter.get("text"));
-				newObject_for_newJsonArray.put("mentions", twitter.get("mentions"));
-				newJsonArray.add(newObject_for_newJsonArray);
-				newObject_for_newJsonArray = new JSONObject();
+			newObject_for_newJsonArray.put("text", twitter.get("text"));
+			newObject_for_newJsonArray.put("mentions", twitter.get("mentions"));
+			newJsonArray.add(newObject_for_newJsonArray);
+			newObject_for_newJsonArray = new JSONObject();
 
-			}
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
 		}
+
 		return newJsonArray;
 
 	}
--- sourceCode/java/sentimentTwitter/src/main/java/sentimentTwitter/TestClass.java
+++ sourceCode/java/sentimentTwitter/src/main/java/sentimentTwitter/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception  {
 		
 		//JSONArray teste = new JSONArray();
 		//JsonFiles jsonFiles = new JsonFiles();
